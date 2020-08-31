1c34e8457a716b7e3368cc80ce1b77e6630d3bf5
Ivanq <imachug@yandex.ru>
2020-08-31 18:59:48 +0300

    v1.2.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
 	name="pdpy11",
-	version="1.2.9",
+	version="1.2.10",
 	packages=find_packages(),
 	long_description=open(join(dirname(__file__), "README.md")).read(),
         long_description_content_type="text/markdown"
